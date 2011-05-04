6571f43ecf06e258c4d4d2279773267bccf48688
Martin Traverso <mtraverso@acm.org>
2011-05-04 16:10:15 -0700

    ServiceResource unit tests

---
+++ src/test/java/com/proofpoint/discovery/TestServiceResource.java
@@ -0,0 +1,61 @@
+package com.proofpoint.discovery;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.proofpoint.node.NodeInfo;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestServiceResource
+{
+    private InMemoryStore store;
+    private ServiceResource resource;
+
+    @BeforeMethod
+    protected void setUp()
+    {
+        store = new InMemoryStore(new DiscoveryConfig());
+        resource = new ServiceResource(store, new NodeInfo("testing"));
+    }
+
+    @Test
+    public void testGetByType()
+    {
+        UUID redNodeId = UUID.randomUUID();
+        Service red1 = new Service(UUID.randomUUID(), redNodeId, "storage", "alpha", "/a/b/c", ImmutableMap.of("key", "1"));
+        Service red2 = new Service(UUID.randomUUID(), redNodeId, "data", "alpha", "/a/b/c", ImmutableMap.of("key", "4"));
+        Service green = new Service(UUID.randomUUID(), UUID.randomUUID(), "storage", "alpha", "/a/b/c", ImmutableMap.of("key", "2"));
+        Service yellow = new Service(UUID.randomUUID(), UUID.randomUUID(), "storage", "beta", "/a/b/c", ImmutableMap.of("key", "3"));
+
+        store.put(redNodeId, ImmutableSet.of(red1, red2));
+        store.put(green.getNodeId(), ImmutableSet.of(green));
+        store.put(yellow.getNodeId(), ImmutableSet.of(yellow));
+
+        assertEquals(resource.getServices("storage"), new Services("testing", ImmutableSet.of(red1, green, yellow)));
+        assertEquals(resource.getServices("data"), new Services("testing", ImmutableSet.of(red2)));
+        assertEquals(resource.getServices("unknown"), new Services("testing", Collections.<Service>emptySet()));
+    }
+
+    @Test
+    public void testGetByTypeAndPool()
+    {
+        UUID redNodeId = UUID.randomUUID();
+        Service red1 = new Service(UUID.randomUUID(), redNodeId, "storage", "alpha", "/a/b/c", ImmutableMap.of("key", "1"));
+        Service red2 = new Service(UUID.randomUUID(), redNodeId, "data", "alpha", "/a/b/c", ImmutableMap.of("key", "4"));
+        Service green = new Service(UUID.randomUUID(), UUID.randomUUID(), "storage", "alpha", "/a/b/c", ImmutableMap.of("key", "2"));
+        Service yellow = new Service(UUID.randomUUID(), UUID.randomUUID(), "storage", "beta", "/a/b/c", ImmutableMap.of("key", "3"));
+
+        store.put(redNodeId, ImmutableSet.of(red1, red2));
+        store.put(green.getNodeId(), ImmutableSet.of(green));
+        store.put(yellow.getNodeId(), ImmutableSet.of(yellow));
+
+        assertEquals(resource.getServices("storage", "alpha"), new Services("testing", ImmutableSet.of(red1, green)));
+        assertEquals(resource.getServices("storage", "beta"), new Services("testing", ImmutableSet.of(yellow)));
+        assertEquals(resource.getServices("storage", "unknown"), new Services("testing", Collections.<Service>emptySet()));
+    }
+}
