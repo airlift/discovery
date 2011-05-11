b64b2d5404949c85ccff659a7b144dfbff01bb62
Martin Traverso <mtraverso@acm.org>
2011-05-10 22:41:52 -0700

    test DiscoveryConfig

--- src/main/java/com/proofpoint/discovery/DiscoveryConfig.java
+++ src/main/java/com/proofpoint/discovery/DiscoveryConfig.java
@@ -3,12 +3,14 @@ package com.proofpoint.discovery;
 import com.proofpoint.configuration.Config;
 import com.proofpoint.units.Duration;
 
+import javax.validation.constraints.NotNull;
 import java.util.concurrent.TimeUnit;
 
 public class DiscoveryConfig
 {
     private Duration maxAge = new Duration(30, TimeUnit.SECONDS);
 
+    @NotNull
     public Duration getMaxAge()
     {
         return maxAge;
---
+++ src/test/java/com/proofpoint/discovery/TestDiscoveryConfig.java
@@ -0,0 +1,44 @@
+package com.proofpoint.discovery;
+
+import com.google.common.collect.ImmutableMap;
+import com.proofpoint.configuration.testing.ConfigAssertions;
+import com.proofpoint.units.Duration;
+import org.testng.annotations.Test;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.proofpoint.discovery.ValidationAssertions.*;
+
+public class TestDiscoveryConfig
+{
+    @Test
+    public void testDefaults()
+    {
+        ConfigAssertions.assertRecordedDefaults(ConfigAssertions.recordDefaults(DiscoveryConfig.class)
+                                                        .setMaxAge(new Duration(30, TimeUnit.SECONDS)));
+    }
+
+    @Test
+    public void testExplicitPropertyMappings()
+    {
+        Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("discovery.max-age", "1m")
+                .build();
+
+        DiscoveryConfig expected = new DiscoveryConfig()
+                .setMaxAge(new Duration(1, TimeUnit.MINUTES));
+
+        ConfigAssertions.assertFullMapping(properties, expected);
+    }
+
+
+    @Test
+    public void testValidatesNotNullDuration()
+    {
+        DiscoveryConfig config = new DiscoveryConfig().setMaxAge(null);
+
+        assertFailedValidation(config, "maxAge", "may not be null", NotNull.class);
+    }
+}
