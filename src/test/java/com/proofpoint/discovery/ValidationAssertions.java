d7611f7da0615c50b2c334da989f28498cf68b4f
Martin Traverso <mtraverso@acm.org>
2011-05-10 22:37:38 -0700

    test CassandraServerConfig

---
+++ src/test/java/com/proofpoint/discovery/ValidationAssertions.java
@@ -0,0 +1,43 @@
+package com.proofpoint.discovery;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+import static java.lang.String.format;
+import static org.testng.Assert.fail;
+
+public class ValidationAssertions
+{
+    private static final Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
+
+    public static <T> void assertFailedValidation(T object, String field, String message, Class<? extends Annotation> annotation)
+    {
+        Set<ConstraintViolation<T>> violations = VALIDATOR.validate(object);
+
+        for (ConstraintViolation<T> violation : violations) {
+            if (annotation.isInstance(violation.getConstraintDescriptor().getAnnotation()) &&
+                violation.getPropertyPath().toString().equals(field)) {
+
+                if (!violation.getMessage().equals(message)) {
+                    fail(format("Expected %s.%s to fail validation for %s with message '%s', but message was '%s'",
+                         object.getClass().getName(),
+                                field,
+                                annotation.getClass().getName(),
+                                message,
+                                violation.getMessage()));
+
+                }
+                return;
+            }
+        }
+
+        fail(format("Expected %s.%s to fail validation for %s with message '%s'",
+             object.getClass().getName(),
+                    field,
+                    annotation.getClass().getName(),
+                    message));
+    }
+}
