f7e70f1391a02933820d78d7e5a81e8ebcfa3b3d
Martin Traverso <mtraverso@acm.org>
2011-05-18 16:48:38 -0700

    id should be parsed as path parameter, not request entity

--- src/main/java/com/proofpoint/discovery/StaticAnnouncementResource.java
+++ src/main/java/com/proofpoint/discovery/StaticAnnouncementResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
@@ -65,7 +66,7 @@ public class StaticAnnouncementResource
 
     @DELETE
     @Path("{id}")
-    public void delete(Id<Service> id)
+    public void delete(@PathParam("id") Id<Service> id)
     {
         store.delete(id);
     }
