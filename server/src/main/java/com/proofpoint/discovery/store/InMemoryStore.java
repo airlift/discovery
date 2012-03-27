b808477a3951b9b7a535a2eb1fe76a24b19ac264
Martin Traverso <mtraverso@acm.org>
2012-03-27 14:42:57 -0700

    make in-memory store concurrent

--- server/src/main/java/com/proofpoint/discovery/store/InMemoryStore.java
+++ server/src/main/java/com/proofpoint/discovery/store/InMemoryStore.java
@@ -1,19 +1,19 @@
 package com.proofpoint.discovery.store;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
 
 import javax.inject.Inject;
 import java.nio.ByteBuffer;
 import java.util.EnumSet;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import static com.proofpoint.discovery.store.Version.Occurs.AFTER;
 import static com.proofpoint.discovery.store.Version.Occurs.SAME;
 
 public class InMemoryStore
 {
-    private final HashMap<ByteBuffer, Entry> map = new HashMap<ByteBuffer, Entry>();
+    private final ConcurrentMap<ByteBuffer, Entry> map = new ConcurrentHashMap<ByteBuffer, Entry>();
     private final ConflictResolver resolver;
 
     @Inject
@@ -26,14 +26,18 @@ public class InMemoryStore
     {
         ByteBuffer key = ByteBuffer.wrap(entry.getKey());
 
-        synchronized (map) {
-            Entry old = map.get(key);
+        boolean done = false;
+        while (!done) {
+            Entry old = map.putIfAbsent(key, entry);
 
+            done = true;
             if (old != null) {
                 entry = resolver.resolve(old, entry);
-            }
 
-            map.put(key, entry);
+                if (entry != old) {
+                    done = map.replace(key, old, entry);
+                }
+            }
         }
     }
 
@@ -41,29 +45,28 @@ public class InMemoryStore
     {
         Preconditions.checkNotNull(key, "key is null");
 
-        synchronized (map) {
-            return map.get(ByteBuffer.wrap(key));
-        }
+        return map.get(ByteBuffer.wrap(key));
     }
 
     public void delete(byte[] key, Version version)
     {
         Preconditions.checkNotNull(key, "key is null");
 
-        synchronized (map) {
-            ByteBuffer wrappedKey = ByteBuffer.wrap(key);
-            
+        ByteBuffer wrappedKey = ByteBuffer.wrap(key);
+
+        boolean done = false;
+        while (!done) {
             Entry old = map.get(wrappedKey);
+
+            done = true;
             if (old != null && EnumSet.of(AFTER, SAME).contains(version.compare(old.getVersion()))) {
-                map.remove(wrappedKey);
+                done = map.remove(wrappedKey, old);
             }
         }
     }
 
     public Iterable<Entry> getAll()
     {
-        synchronized (map) {
-            return ImmutableList.copyOf(map.values());
-        }
+        return map.values();
     }
 }
